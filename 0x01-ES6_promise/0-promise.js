b01ba8e592e9184d882560f6fb7ac7b1d482235b
Anwar <anwarsadat.d2@gmail.com>
2023-04-19 23:27:37 +0000

    Promises

---
+++ 0x01-ES6_promise/0-promise.js
@@ -0,0 +1,4 @@
+export default function getResponseFromAPI(){
+    var promise = new Promise( () => {} )
+    return promise;
+}
---
+++ 0x01-ES6_promise/main.js
@@ -0,0 +1,5 @@
+import getFullResponseFromAPI from './1-promise';
+
+
+console.log(getFullResponseFromAPI(false));
+console.log(getFullResponseFromAPI(true));
