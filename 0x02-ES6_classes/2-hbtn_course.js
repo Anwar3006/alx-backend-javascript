3f587d3b61c77b3c332465bec3c1b851cabd22c1
Anwar <anwarsadat.d2@gmail.com>
2023-04-22 13:41:51 +0000

    Classes

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,36 +1,64 @@
+/**
+ * Represents a Holberton Course.
+ */
 export default class HolbertonCourse {
+  /**
+   * Creates a new @see {@link HolbertonCourse}.
+   *
+   * @param {String} name - The name of the course.
+   * @param {Number} length - How long the course is (in months).
+   * @param {String[]} students - The names of students in the course.
+   */
   constructor(name, length, students) {
-    this._name = name;
-    this._length = length;
-    this._students = students;
+    this.name = name;
+    this.length = length;
+    this.students = students;
   }
 
-  get name() { return this._name; }
+  /**
+   * Gets the name of this course.
+   */
+  get name() {
+    return this._name;
+  }
 
-  set name(newName) {
-    if (typeof (newName) === 'string') {
-      this._name = newName;
-    } else if (newName === '') {
-      throw new Error('Name can not be empty');
-    } else {
+  /**
+   * Sets the name of this course.
+   */
+  set name(value) {
+    if (typeof value !== 'string') {
       throw new TypeError('Name must be a string');
     }
+    this._name = value;
   }
 
-  get length() { return this._length; }
+  /**
+   * Gets the length of this course (in months).
+   */
+  get length() {
+    return this._length;
+  }
 
-  set length(newLength) {
-    if (typeof (newLength) === 'number') {
-      this._name = newLength;
-    } else if (newLength === '') {
-      throw new Error('Length can not be empty');
-    } else {
+  /**
+   * Sets the length of this course (in months).
+   */
+  set length(value) {
+    if (typeof value !== 'number') {
       throw new TypeError('Length must be a number');
     }
+    this._length = value;
   }
 
-  get students() { return this._students; }
+  /**
+   * Gets the names of students in this course.
+   */
+  get students() {
+    return this._students;
+  }
 
+  /**
+   * Sets the names of students in this course.
+   */
   set students(value) {
     if (!(value instanceof Array)) {
       throw new TypeError('Students must be an array of strings');
