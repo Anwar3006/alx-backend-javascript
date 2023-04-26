d6e8c2d02adac2c27597f1bc4eba552d0c2d0ca9
Anwar <anwarsadat.d2@gmail.com>
2023-04-26 00:06:29 +0000

    data manipulations

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,3 +1,11 @@
+/**
+ * Retrieves ids from a list of students.
+ * @param {{
+*   id: Number,
+*   firstName: String,
+*   location: String
+* }[]} students - The list of students.
+*/
 export default function getListStudentIds(array1) {
   if (typeof array1 !== 'object') {
     return [];
