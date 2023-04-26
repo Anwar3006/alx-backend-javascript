cde9e41b0ba4213831635cb47dc88a3b1eb38442
Anwar <anwarsadat.d2@gmail.com>
2023-04-26 22:28:10 +0000

    Data Manipulation

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -2,16 +2,18 @@ export default function cleanSet(mySet, startString) {
   // mySet has values that begin with startString
   // remove the occurrence of startString from all values
   // create a string with the updated values separated by -
-  if (typeof startString === 'string') {
-    let myStr = [];
-    mySet.forEach((value) => {
-      if (value.startsWith(startString)) {
-        const slen = startString.length;
-        element = `${value.slice(slen, value.length)}`;
-        myStr.push(element);
-      }
-    });
-    return myStr.join('-');
+  const parts = [];
+  if (!set || !startString || !(set instanceof Set) || typeof startString !== 'string') {
+    return '';
   }
-  return '';
+  for (const value of set.values()) {
+    if (typeof value === 'string' && value.startsWith(startString)) {
+      const valueSubStr = value.substring(startString.length);
+
+      if (valueSubStr && valueSubStr !== value) {
+        parts.push(valueSubStr);
+      }
+    }
+  }
+  return parts.join('-');
 }
