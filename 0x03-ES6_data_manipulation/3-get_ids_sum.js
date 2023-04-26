adb19eba4ab6619aa983649182f6e260017c1205
Anwar <anwarsadat.d2@gmail.com>
2023-04-26 23:10:14 +0000

    Data Manipulation

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,6 +1,6 @@
 export default function getStudentIdsSum(array1) {
   if (typeof array1 !== 'object') {
-    throw new TypeError('First argument must be a list');
+    return 0;
   }
   const initialValue = 0;
   return array1.reduce((accumulator, currentValue) => accumulator.id || accumulator + currentValue.id, initialValue);
