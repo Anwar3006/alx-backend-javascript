b3a60eda39b990b3c62f035989e8f0f139871ab1
Anwar <anwarsadat.d2@gmail.com>
2023-04-27 12:31:56 +0000

    TypeScript Intro

---
+++ 0x04-TypeScript/task_4/js/main.ts
@@ -0,0 +1,30 @@
+/**
+ * create and export a constant cpp for Cpp Subjects
+create and export a constant java for Java Subjects
+create and export a constant react for React Subjects
+create and export one Teacher object cTeacher with experienceTeachingC = 10
+for Cpp subject, log to the console C++, set cTeacher as the teacher, call the two methods getRequirements and getAvailableTeacher and print the strings they return
+ */
+export const cpp: Subjects.Cpp = new Subjects.Cpp();
+export const java: Subjects.Java = new Subjects.Java();
+export const react: Subjects.React = new Subjects.React();
+export const cTeacher: Subjects.Teacher = {
+    firstName: "Soren",
+    lastName: "Kierkegaard",
+    experienceTeachingC: 10
+};
+
+console.log('C++')
+cpp.setTeacher = cTeacher
+console.log(cpp.getRequirements())
+console.log(cpp.getAvailableTeacher())
+
+console.log('Java')
+java.setTeacher = cTeacher
+console.log(java.getRequirements())
+console.log(java.getAvailableTeacher())
+
+console.log('React')
+react.setTeacher = cTeacher
+console.log(react.getRequirements())
+console.log(react.getAvailableTeacher())
