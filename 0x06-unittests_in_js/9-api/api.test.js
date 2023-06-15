2957c2e41afbb3d4ac3ef613495e17c32a6ef3b3
Anwar3006 <anwarsadat.d2@gmail.com>
2023-06-15 21:13:49 +0000

    nodejs unittests

--- 0x06-unittests_in_js/9-api/api.test.js
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -1,4 +1,4 @@
-var expect = require('chai')
+var {expect} = require('chai')
 const request = require('request')
 
 describe('Testing the index page', () => {
@@ -14,12 +14,11 @@ describe('Testing the index page', () => {
     });
     describe('GET /cart/:id', () => {
         it('should return a correct status code', (done) => {
-            request.get('http://localhost:7865/cart/34)', (err, res, body) => {
+            request.get('http://localhost:7865/cart/34', (err, res, body) => {
                 expect(res.statusCode).to.be.equal(200);
                 expect(body).to.be.equal('Payment method for cart: 34');
-                done()
             })
-            request.get('http://localhost:7865/cart/bob)', (err, res, body) => {
+            request.get('http://localhost:7865/cart/bob', (err, res, body) => {
                 expect(res.statusCode).to.be.equal(404);
                 done()
             })
